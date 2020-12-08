6b6e8b102d8c3562b991ca209758ba7ae95c856b
xiangyang <xiangyang@growingio.com>
2020-12-08 20:27:39 +0800

    docs: 完善对外接口的javadoc

--- growingio-tracker-cdp/src/main/java/com/growingio/android/sdk/track/IGrowingTracker.java
+++ growingio-tracker-cdp/src/main/java/com/growingio/android/sdk/track/IGrowingTracker.java
@@ -28,22 +28,74 @@ import java.util.Map;
  */
 public interface IGrowingTracker {
 
+    /**
+     * 发送一个自定义事件。
+     * 在添加所需要发送的事件代码之前，需要在事件管理用户界面配置事件以及事件级变量。
+     *
+     * @param eventName 事件名，事件标识符
+     */
     void trackCustomEvent(String eventName);
 
+    /**
+     * 发送一个自定义事件。
+     * 在添加所需要发送的事件代码之前，需要在事件管理用户界面配置事件以及事件级变量。
+     *
+     * @param eventName  事件名，事件标识符
+     * @param attributes 事件发生时所伴随的维度信息
+     */
     void trackCustomEvent(String eventName, Map<String, String> attributes);
 
+    /**
+     * 发送一个自定义事件。
+     * 在添加所需要发送的事件代码之前，需要在事件管理用户界面配置事件以及事件级变量。
+     *
+     * @param eventName 事件名，事件标识符
+     * @param itemKey   事件发生关联的物品模型Key
+     * @param itemId    事件发生关联的物品模型ID
+     */
     void trackCustomEvent(String eventName, String itemKey, String itemId);
 
+    /**
+     * 发送一个自定义事件。
+     * 在添加所需要发送的事件代码之前，需要在事件管理用户界面配置事件以及事件级变量。
+     *
+     * @param eventName  事件名，事件标识符
+     * @param attributes 事件发生时所伴随的维度信息
+     * @param itemKey    事件发生关联的物品模型Key
+     * @param itemId     事件发生关联的物品模型ID
+     */
     void trackCustomEvent(String eventName, Map<String, String> attributes, String itemKey, String itemId);
 
+    /**
+     * 以登录用户的身份定义用户属性变量，用于用户信息相关分析。
+     *
+     * @param attributes 用户属性信息
+     */
     void setLoginUserAttributes(Map<String, String> attributes);
 
+    /**
+     * 当用户登录之后调用，设置登录用户ID
+     *
+     * @param userId 用户ID，长度限制大于0且小于等于1000，如果大于长度1000将只截取前1000长度
+     */
     void setLoginUserId(String userId);
 
+    /**
+     * 当用户登出之后调用，清除已经设置的登录用户ID。
+     */
     void cleanLoginUserId();
 
+    /**
+     * 设置用户当前的地理位置，基于WGS-84坐标
+     *
+     * @param latitude  地理坐标点纬度
+     * @param longitude 地理坐标点经度
+     */
     void setLocation(double latitude, double longitude);
 
+    /**
+     * 清除用户当前的地理位置
+     */
     void cleanLocation();
 
     /**
@@ -55,9 +107,9 @@ public interface IGrowingTracker {
     void setDataCollectionEnabled(boolean enabled);
 
     /**
-     * 异步获取deviceId
+     * 异步获取deviceId，又称为匿名用户id，SDK 自动生成用来定义唯一设备。
      *
-     * @param callback 对应回调,  回调线程不确定
+     * @param callback 参数非空，回调线程不确定
      */
     void getDeviceId(@Nullable ResultCallback<String> callback);
 }
