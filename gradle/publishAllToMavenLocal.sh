537e8f2197ff36681264e6b73854537afbd486b0
shenliming <348515494@qq.com>
2021-04-13 15:23:17 +0800

    update ci in cdp

--- gradle/publishAllToMavenLocal.sh
+++ gradle/publishAllToMavenLocal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ./gradlew clean \
-&& ./gradlew :growingio-tracker-core-cdp:publishReleaseAgentPublicationToMavenLocal \
-&& ./gradlew :growingio-tracker-cdp:publishReleaseAgentPublicationToMavenLocal \
-&& ./gradlew :growingio-autotracker-cdp:publishReleaseAgentPublicationToMavenLocal \
+&& ./gradlew :growingio-tracker-core-cdp:publishMavenAgentPublicationToMavenLocal \
+&& ./gradlew :growingio-tracker-cdp:publishMavenAgentPublicationToMavenLocal \
+&& ./gradlew :growingio-autotracker-cdp:publishMavenAgentPublicationToMavenLocal \
 && ./gradlew clean
